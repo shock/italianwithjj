3d93b80c65d88500abf95171a9949c7a5aab5a35
Bill Doughty <bdoughty@yahoo.com>
2009-07-03 00:01:24 -0500

    added graphics. formatting changes.  text edits.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090701015600) do
+ActiveRecord::Schema.define(:version => 20090703042100) do
 
   create_table "students", :force => true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20090701015600) do
     t.integer  "hourly_rate"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "comments"
   end
 
 end
